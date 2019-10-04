b10debb4e28271039daaa4417a76cda8a2413de3
Austen Cameron <austen.cameron@gmail.com>
2019-10-04 13:01:28 -0600

    added dropdown

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-require('./bootstrap');
+require('./bootstrap')
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    let dropdowns = document.querySelectorAll('.dropdown')
+
+    let closeDropdowns = () => {
+        dropdowns.forEach(el => {
+            el.classList.remove('open')
+        })
+    }
+
+    dropdowns.forEach(el => {
+        el.querySelector('.btn-details').addEventListener('click', () => {
+            el.classList.toggle('open')
+        })
+
+        el.querySelector('.overlay').addEventListener('click', closeDropdowns)
+    })
+
+    document.addEventListener('keyup', event => {
+        if (event.code === 'Escape') {
+            closeDropdowns()
+        }
+    })
+})
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body class="font-sans antialiased text-gray-700 bg-gray-100">
-        <div class="p-4">
+        <div class="p-4 container mx-auto">
             <table class="table-responsive">
                 <thead>
                     <tr>
@@ -39,11 +39,27 @@
                                 @endif
                             </td>
                             <td class="actions">
-                                <a href="#subscriber-edit" class="btn-details">
-                                    <div class="dot"></div>
-                                    <div class="dot"></div>
-                                    <div class="dot"></div>
-                                </a>
+                               
+                                <div class="dropdown flex flex-col items-end">
+                                    <a href="#subscriber-edit" class="btn-details">
+                                        <div class="dot"></div>
+                                        <div class="dot"></div>
+                                        <div class="dot"></div>
+                                    </a>
+
+                                    <button class="overlay z-10"></button>
+                                    <div class="dropdown-menu absolute z-20 w-48 py-2 mt-8 bg-gray-600 rounded-lg shadow-xl">
+                                        <a href="subscribers/{{ $subscriber->id }}" class="block p-2 px-3 text-gray-300 hover:text-white hover:bg-indigo-600">
+                                            Edit Subscriber
+                                        </a>
+                                        <a href="#cool" class="block p-2 px-3 text-gray-300 hover:text-white hover:bg-indigo-600">
+                                            Cool Links
+                                        </a>
+                                        <a href="#links" class="block p-2 px-3 text-gray-300 hover:text-white hover:bg-indigo-600">
+                                            In The Dropdown
+                                        </a>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     @endforeach
